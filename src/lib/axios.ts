61312796b2058e484ee45f60ec6e51a39d8af1e4
João Andrade <joaovictorvieira.23@hotmail.com>
2023-01-27 12:48:27 -0300

    fix: change baseUrl

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://mks-challenge-api-frontend.herokuapp.com/api/v1'
+    baseURL: 'https://mks-challenge-api-frontend.herokuapp.com/api/v1'
 });
